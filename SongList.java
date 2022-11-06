3d55aa57f35b46b2b07d4f49c3a1840dd2d3ef6b
Dipesh Tamang <Lamadepesh10@gmail.com>
2022-11-06 11:51:57 +0545

    [ADD] print and get popular song

--- SongList.java
+++ SongList.java
@@ -20,6 +20,36 @@ public class SongList {
         Songs.remove(index);
         System.out.println("The song has been removed sucessfully");
     }
+
+    public void printSongs()
+    {
+
+        if(Songs.size() == 0)
+        {
+            System.out.println("There are no songs here!");
+            return;
+        }
+        
+        System.out.println("Index\tArtist\tTitle");
+        for (int i = 0; i < Songs.size(); i++)
+        {
+            Song s = Songs.get(i);
+            int index = Songs.indexOf(s);
+
+            System.out.println(index+"\t"+s.getArtist()+"\t"+s.getTitle());
+        
+        }
+    }
+
+    public void getPopular(int play)
+    {
+        List<Song> popularSongs = Songs.stream()
+                                        .filter(p -> p.getplayCount() >= play).collect(Collectors.toList());
+        for (Song s : popularSongs
+                        ) {
+                            System.out.println(s.getTitle() + "\t\t\t\t\t" + s.getArtist() + "\t\t\t\t\t" + s.getplayCount());
+                        }
+    }
    
 
 }
