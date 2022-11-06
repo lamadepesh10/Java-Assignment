9415b9c7d94ff0b4084659d8040471752f1eff51
Dipesh Tamang <Lamadepesh10@gmail.com>
2022-11-06 11:49:02 +0545

    [ADD] Main function to MainSong

---
+++ MainSong.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+  
+class MainSong extends Process{
+    public static void main(String[] args) 
+    {
+        
+    }
+
+    
+
+}
+
+
+
+  
+
